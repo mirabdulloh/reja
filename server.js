861957c59eeadb939b25abb167c3602caff7094a
mirabdulloh <tursunovmirabdulla@gmail.com>
2025-09-18 06:17:04 -0700

    feat: first ejs views folders

--- server.js
+++ server.js
@@ -20,11 +20,19 @@ app.set("view engine", "ejs");
 
 // 4 -- Routing ga bogliq qodlar
 
-app.get("/hello", function(req, res){
-    res.end(`<h1 style="background:red">Hello world</h1>`)
+// app.get("/hello", function(req, res){
+//     res.end(`<h1 style="background:red">Hello world</h1>`)
+// });
+// app.get("/gift", function(req, res){
+//     res.end(`<h1 style="background:red">You are in gift page</h1>`)
+// });
+
+app.post("/create-item",(req,res)=>{
+    console.log(req.body);
+    res.json({test:"success"});
 });
-app.get("/gift", function(req, res){
-    res.end(`<h1 style="background:red">You are in gift page</h1>`)
+app.get("/", function(req, res){
+    res.render("harid");
 });
 
 const server = http.createServer(app);
